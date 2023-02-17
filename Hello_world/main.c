7425ff645b56fb399a6c3534ad1fc25d3808f059
Kabelo Mokhethi <mokhethikabelo@gmail.com>
2023-02-17 10:49:03 +0200

    This if my first c program

---
+++ Hello_world/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("\nHello world!\n");
+    int a = 10;
+    int b = 20;
+    printf("\nThe answer is: %d\n", a + b);
+    return 0;
+}
