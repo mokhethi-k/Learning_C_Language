8f029fe957bb153c6be479bbff4160f1d184dbd2
Kabelo Mokhethi <mokhethikabelo@gmail.com>
2023-02-17 15:49:11 +0200

    The switch statement is similar to if else statement as it is also for multiple decision making.

---
+++ Switch_statements/main.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+########################################################################################
+            switch statement can be used for multiple decision making structure
+            just like if else statement but it is cleaner than if else statement.
+########################################################################################
+*/
+
+void check_grade(char grade){
+    switch(grade){
+        case 'A': printf("\nYou got 80-89%.\n");
+        break;
+
+        case 'B': printf("\nYou got 70-79%.\n");
+        break;
+
+        case 'C': printf("\nYou got 60-69%.\n");
+        break;
+
+        case 'D': printf("\nYou got 50-59%.\n.");
+        break;
+
+        case 'E': printf("\nYou failed horribly!!!");
+        break;
+
+        default: printf("\nYou probably didn\'t enter a valid grade.\n");
+        break;
+    }
+}
+
+int main()
+{
+    int grade;
+    printf("\nPlease enter a grade to be checked:");
+    scanf(" %c",&grade);// Leave a space behind format specifier to be able to take char as an input.
+    check_grade(toupper(grade));
+    return 0;
+}
