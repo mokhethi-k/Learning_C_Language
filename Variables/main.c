c81b16aaadb05d887ebc031f4461c52b78650ae2
Kabelo Mokhethi <mokhethikabelo@gmail.com>
2023-02-17 10:56:07 +0200

    This is how variables that store different data types are initialized in C.

---
+++ Variables/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char name[] = "Ted";
+    int age = 40;
+
+    printf("\nThere once was a man named %s.\n",name);
+    printf("He was %d years old.\n",age);
+    printf("He really liked the name %s.\n",name);
+    printf("But did not like being %d.\n",age);
+
+    return 0;
+}
