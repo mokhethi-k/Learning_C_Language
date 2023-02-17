c3846da6d77805fc606cc95e693e014f2cfd00b1
Kabelo Mokhethi <mokhethikabelo@gmail.com>
2023-02-17 14:36:35 +0200

    This program shows how C takes care of constants

---
+++ Constants/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    /*
+    Constant is a variable that cannot be modified
+    and the variables are mostly in capital letters.
+    */
+    const int NUM = 5;
+    printf("\nThe number %d cannot be modified.\n",NUM);
+    return 0;
+}
